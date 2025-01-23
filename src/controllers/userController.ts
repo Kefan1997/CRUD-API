a4d67e1eaccdc20eaee32905410cd3ef6c4bfed1
Oleg Rashchinksy <oleg.rashchinksy@sharpgaming.com>
2025-01-23 16:57:52 +0100

    feat: use file where we save users

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,20 +1,26 @@
 import { Request, Response } from 'express';
-import { User, Users } from '../types/response';
 import { v4 as uuidv4, validate } from 'uuid';
+import { User } from '../types/response';
+import { getUsersFromFile, writeUsersToFile } from '../helpers';
 
-const users: Users = {};
-
-export const getUsers = (req: Request, res: Response) => {
-  res.status(200).json(users);
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await getUsersFromFile();
+    res.status(200).json(users);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
-export const getUserById = (req: Request, res: Response): void => {
+export const getUserById = async (req: Request, res: Response): Promise<void> => {
   const { userId } = req.params;
   if (!validate(userId)) {
     res.status(400).json({ message: 'Invalid user ID format' });
     return;
   }
 
+  const users = await getUsersFromFile();
   const user = users[userId];
 
   if (!user) {
@@ -25,25 +31,29 @@ export const getUserById = (req: Request, res: Response): void => {
   res.status(200).json(user);
 };
 
-export const createUser = (req: Request, res: Response): void => {
+export const createUser = async (req: Request, res: Response): Promise<void> => {
   const { name, email, age } = req.body;
   if (!name || !email || typeof age !== 'number') {
     res.status(400).json({ message: 'Missing required fields' });
     return;
   }
-
+  const users = await getUsersFromFile();
   const newUser: User = { id: uuidv4(), name, email, age };
   users[newUser.id] = newUser;
+
+  await writeUsersToFile(users);
+
   res.status(201).json(newUser);
 };
 
-export const updateUser = (req: Request, res: Response): void => {
+export const updateUser = async (req: Request, res: Response): Promise<void> => {
   const { userId } = req.params;
   if (!validate(userId)) {
     res.status(400).json({ message: 'Invalid user ID format' });
     return;
   }
 
+  const users = await getUsersFromFile();
   const user = users[userId];
 
   if (!user) {
@@ -59,16 +69,19 @@ export const updateUser = (req: Request, res: Response): void => {
     age: age || user.age,
   };
 
-  res.status(200).json(users[userId]);
+  await writeUsersToFile(users);
+
+  res.status(200).json((await getUsersFromFile())[userId]);
 };
 
-export const deleteUser = (req: Request, res: Response): void => {
+export const deleteUser = async (req: Request, res: Response): Promise<void> => {
   const { userId } = req.params;
   if (!validate(userId)) {
     res.status(400).json({ message: 'Invalid user ID format' });
     return;
   }
 
+  const users = await getUsersFromFile();
   const user = users[userId];
 
   if (!user) {
@@ -76,5 +89,7 @@ export const deleteUser = (req: Request, res: Response): void => {
   }
 
   delete users[userId];
+  await writeUsersToFile(users);
+
   res.status(204).send();
 };
