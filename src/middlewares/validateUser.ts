0bb2ed7676bf82c7d92513fd5d307a9bd0e4ccb1
Oleg Rashchinksy <oleg.rashchinksy@sharpgaming.com>
2025-03-18 12:15:34 +0100

    feat: add Joi for schema validation

--- src/middlewares/validateUser.ts
+++ src/middlewares/validateUser.ts
@@ -1,11 +1,21 @@
-import { Request, Response, NextFunction } from "express";
-import { validate } from "uuid";
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+import Joi from 'joi';
+import { validate } from 'uuid';
+
+const userSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  age: Joi.number().required(),
+});
 
 export function validateUser(req: Request, res: Response, next: NextFunction) {
-  const {name, email, age} = req.body;
+  const { error } = userSchema.validate(req.body, { abortEarly: false });
 
-  if(!name || !email || typeof age !=='number') {
-    res.status(400).json({message: 'Missing required fields'});
+  if (error) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ errorCode: ReasonPhrases.BAD_REQUEST, message: 'Missing required fields' });
     return;
   }
 
@@ -13,10 +23,12 @@ export function validateUser(req: Request, res: Response, next: NextFunction) {
 }
 
 export function validateUserId(req: Request, res: Response, next: NextFunction) {
-  const {userId} = req.params;
+  const { userId } = req.params;
 
-  if(!validate(userId)) {
-    res.status(400).json({ message: 'Invalid user ID format' });
+  if (!validate(userId)) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ errorCode: ReasonPhrases.BAD_REQUEST, message: 'Invalid user ID format' });
     return;
   }
 
