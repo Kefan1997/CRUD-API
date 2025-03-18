621b34591268cb0c5128df9e4788fd98bd6dadbb
Oleg Rashchinksy <oleg.rashchinksy@sharpgaming.com>
2025-03-18 11:54:23 +0100

    fix: rename file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import userRouter from './routers/userRouter';
+import userRouter from './routers/user';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
