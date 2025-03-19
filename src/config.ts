d33a5e8d78eb6b35204ab419ddf2a8f0a4056be2
Oleg Rashchinksy <oleg.rashchinksy@sharpgaming.com>
2025-03-19 10:58:31 +0100

    feat: add validation for configuration

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,9 @@ import path from 'node:path';
 dotenv.config();
 
 export const DATA_STORAGE = process.env.DATA_STORAGE || 'sqlite';
-export const MOCK_USERS_DATA_PATH = path.resolve(
-  process.env.MOCK_USERS_DATA_PATH || 'src/mocks/users.json'
-);
+
+if (!process.env.MOCK_USERS_DATA_PATH) {
+  throw new Error('MOCK_USERS_DATA_PATH is required in the environment variables');
+}
+
+export const MOCK_USERS_DATA_PATH = path.resolve(process.env.MOCK_USERS_DATA_PATH);
